2704c0223bc0328b732a4d61830a517c64cfb244
Nyholm <tobias.nyholm@gmail.com>
2019-06-26 23:15:22 +0200

    Bugfixes and minor issues

--- src/Consumer/SnsConsumer.php
+++ src/Consumer/SnsConsumer.php
@@ -2,33 +2,53 @@
 
 declare(strict_types=1);
 
-namespace App\Consumer\SnsConsumer;
+namespace App\Consumer;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Event\WorkerMessageReceivedEvent;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\ReceivedStamp;
 use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 final class SnsConsumer
 {
     private $bus;
     private $serializer;
+    private $transportName;
     private $logger;
+    private $eventDispatcher;
 
     public function __construct(
         MessageBusInterface $bus,
         SerializerInterface $serializer,
-        LoggerInterface $logger
+        string $transportName = 'sns',
+        LoggerInterface $logger = null,
+        EventDispatcherInterface $eventDispatcher = null
     ) {
+
         $this->bus = $bus;
         $this->serializer = $serializer;
+        $this->transportName = $transportName;
         $this->logger = $logger;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function consume(array $event)
     {
         try {
             $envelope = $this->serializer->decode(['body' => $event['Message']]);
-            $this->bus->dispatch($envelope);
+
+            $sfEvent = new WorkerMessageReceivedEvent($envelope, $this->transportName);
+            if ($this->eventDispatcher !== null) {
+                $this->eventDispatcher->dispatch($sfEvent);
+            }
+
+            if (!$sfEvent->shouldHandle()) {
+                return;
+            }
+
+            $this->bus->dispatch($envelope->with(new ReceivedStamp($this->transportName)));
         } catch (\Throwable $e) {
             $retryAttempt = (int) ($event['MessageAttributes']['retry_attempt']['Value'] ?? 0);
             $this->logger->critical('Could not consume message from SNS.', [
